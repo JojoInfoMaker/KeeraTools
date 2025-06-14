0d539885ac6c77db8f55cc38ee6e2ba8f6f0a01f
Jojo - InfoMaker <31yemel@gmail.com>
2025-06-14 22:15:55 +0200

    Modif install.sh

--- Install Nova/Install Linux/Install.sh
+++ Install Nova/Install Linux/Install.sh
@@ -1,6 +1,6 @@
-chmod +x AppV7.x86_64 #autorise AppV7.x86_64 à être éxécuté en double clique
+chmod +x Nova-Installer.x86_64 #autorise AppV7.x86_64 à être éxécuté en double clique
 
-sudo cp -i AppV7.x86_64 ~/.local/bin #bouge AppV7.x86_64 dans /home/$USER/.local/bin (pour que le .desktop trouve super vite et universelement)
+sudo cp -i Nova-Installer.x86_64 ~/.local/bin #bouge AppV7.x86_64 dans /home/$USER/.local/bin (pour que le .desktop trouve super vite et universelement)
 
 mkdir ~/.local/share/icons/Nova-Installer #créer un dossier pour les icones
 cd data
@@ -10,7 +10,7 @@ cd -
 touch Nova-Installer.desktop
 echo [Desktop Entry] >> Nova-Installer.desktop
 echo Name="Nova Installer" >> Nova-Installer.desktop
-echo Exec=~/.local/bin/AppV7.x86_64 >> Nova-Installer.desktop
+echo Exec=~/.local/bin/Nova-Installer.x86_64 >> Nova-Installer.desktop
 echo Icon=~/.local/share/icons/Nova-Installer/inco.png >> Nova-Installer.desktop #s'occupe de créer d'écrire dans le .desktop
 echo Type=Application >> Nova-Installer.desktop
 echo Terminal=false >> Nova-Installer.desktop
